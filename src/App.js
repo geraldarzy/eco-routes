feef747c2672c03d3e00752092439516e8feb4fd
geraldarzy <geraldarzola@gmail.com>
2021-05-07 22:51:31 -0400

    add Map component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Map from './components/Map'
 
 function App() {
   return (
-  
+    <div>
+      <Map/>
+    </div>
   );
 }
-
+// http://localhost:3000/origin-destination/-73;8891016/40;7410592/-73;9896416/40;7410592
 export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Map extends React.Component{
+    constructor(props){
+        super();
+    }
+
+    render(){
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+export default Map;
