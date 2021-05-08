d72874f86f1b674bf45a69a0a994fd450ad768c7
geraldarzy <geraldarzola@gmail.com>
2021-05-08 13:21:15 -0400

    set initial state for map settings

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,6 +4,12 @@ import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 class Map extends React.Component{
     constructor(props){
         super();
+        this.state = {
+            lng: -73.9896416,
+            lat: 40.7410592,
+            zoom: 12
+          };
+          this.mapContainer = React.createRef();
     }
 
     render(){
