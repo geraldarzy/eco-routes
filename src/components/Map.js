cbdd207a8dcfc722988ae72f4212d2ce99879379
geraldarzy <geraldarzola@gmail.com>
2021-05-09 14:15:14 -0400

    add arbitrary link in Map Component to fetch backend

--- src/components/Map.js
+++ src/components/Map.js
@@ -17,8 +17,8 @@ class Map extends React.Component{
         const map = new mapboxgl.Map({
           container: 'map-container',
           style: 'mapbox://styles/mapbox/streets-v11',
-          center: [-122.486052, 37.830348],
-          zoom: 15
+          center: [lng, lat],
+          zoom: zoom
         });
         map.on('move', () => {
             let h = map.getCenter();
@@ -32,42 +32,50 @@ class Map extends React.Component{
 
 
     //  ___________________________________________
-    // fetch(url).then(resp=>resp.json()).then(json=>{
+    let url = 'https://api.mapbox.com/directions/v5/mapbox/driving/-73.8891016,40.7410592;-73.9896416,40.7410592?geometries=geojson&access_token=pk.eyJ1IjoiYXJ6eSIsImEiOiJja284d25iODAyNm1oMnJtYjVnZmllaG1zIn0.P8siHAbm84-8l4B0b723qg'
+    fetch(url).then(resp=>resp.json()).then(json=>{
 
-    //     let coordinates = json.routes[0].geometry.coordinates;
-    //     // if(map.getSource){
-    //         //     map.removeLayer('route');
-    //         //     map.removeSource('route');
-    //         // }
-    //         map.addSource('route', {
-    //             'type': 'geojson',
-    //             'data': {
-    //                 'type': 'Feature',
-    //                 'properties': {},
-    //                 'geometry': {
-    //                     'type': 'LineString',
-    //                     'coordinates': coordinates
-    //                 }
-    //             }
-    //         });
-    //         map.addLayer({
-    //             'id': 'route',
-    //             'type': 'line',
-    //             'source': 'route',
-    //             'layout': {
-    //                 'line-join': 'round',
-    //                 'line-cap': 'round'
-    //             },
-    //             'paint': {
-    //                 'line-color': '#888',
-    //                 'line-width': 8
-    //             }
-    //         });
-    //     })
+        let coordinates = json.routes[0].geometry.coordinates;
+        // if(map.getSource){
+        //         map.removeLayer('route');
+        //         map.removeSource('route');
+        //     }
+            map.addSource('route', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'Feature',
+                    'properties': {},
+                    'geometry': {
+                        'type': 'LineString',
+                        'coordinates': coordinates
+                    }
+                }
+            });
+            map.addLayer({
+                'id': 'route',
+                'type': 'line',
+                'source': 'route',
+                'layout': {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                'paint': {
+                    'line-color': '#888',
+                    'line-width': 8
+                }
+            });
+        })
         
     //  ___________________________________________
+    const flyTo=()=>{
+        map.fitBounds([[lng,lat],[-63.9896416,40.7410592]])
+    }
     return (
         <div>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <button onClick={flyTo}>click me to fly</button>
         </div>
         )
     }
