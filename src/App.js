c70086ab1fbae4f10be6c34c3562088d0f00e804
geraldarzy <geraldarzola@gmail.com>
2021-05-10 20:26:54 -0400

    import Form into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Map from './components/Map'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App App-header">
+      <Form/>
       <Map/>
     </div>
   );
