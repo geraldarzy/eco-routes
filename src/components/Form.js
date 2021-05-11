fbb59219932a805ebaca0e4a6eed72f912fdb4a6
geraldarzy <geraldarzola@gmail.com>
2021-05-10 22:03:26 -0400

    add controlled form with state

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,17 +3,31 @@ import React from 'react';
 class Form extends React.Component{
     constructor(props){
         super();
+        this.state={
+            origin:'',
+            destination:''
+        }
     }
 
+    handleChange = (e) =>{
+        //handles input change dynamically by producing a new obj with target.id 
+        //we can conver this to es6 with object.assign
+        let stateObject = function() {
+            let obj = {};
+            obj[e.target.id] = e.target.value;
+            return obj;
+        }
+        
+        this.setState( stateObject );
+    }
     render(){
-
         return (
             <form>
                 <label for='origin'>Origin:</label>
-                <input id='origin'type='text'></input>
+                <input onChange={this.handleChange} id='origin'type='text'></input>
                 <br/>
                 <label for='destination'>Destination:</label>
-                <input id='destination'type='text'></input>
+                <input onChange={this.handleChange} id='destination'type='text'></input>
             </form>
         )
     }
