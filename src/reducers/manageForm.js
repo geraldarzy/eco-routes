483d3574a0562158b358837a345cb02c0da197b2
geraldarzy <geraldarzola@gmail.com>
2021-05-11 11:44:26 -0400

    depending on what action is received by reducer, different changes are made to store

--- src/reducers/manageForm.js
+++ src/reducers/manageForm.js
@@ -1,4 +1,26 @@
 function manageForm(
-    
+    state={
+        oirigin:'',
+        destination:''
+    }, action
+) {
+    switch (action.type){
+        case 'CHANGE_ORIGIN':
+            return{
+                ...state,
+                origin: action.origin,
+                destination: state.destination
+            }
+        case 'CHANGE_DESTINATION':
+            return{
+                ...state,
+                origin: state.origin,
+                destination: action.destination
+            }
+
+        default:
+            return state
+        }
+}
 
 export default manageForm;
