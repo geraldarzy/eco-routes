80ed6fa85121a7fee40161dcbbeee51bb316e53c
geraldarzy <geraldarzola@gmail.com>
2021-05-12 00:23:02 -0400

    change handleInputChange to be dynamic by taking in event as param instead

--- src/components/MapLogic.js
+++ src/components/MapLogic.js
@@ -10,11 +10,16 @@ class MapLogic extends React.Component{
             destination: ''
         }
     }
-    handleInputChange=(origin,destination)=>{
-        this.setState({
-            origin:origin,
-            destination:destination
-        });
+    handleInputChange=(e)=>{
+        //handles input change dynamically by producing a new obj with target.id 
+        //we can convert this to es6 with object.assign
+        let stateObject = function() {
+            let obj = {};
+            obj[e.target.id] = e.target.value;
+            return obj;
+        }
+        
+        this.setState( stateObject );
     }
 
     render(){
