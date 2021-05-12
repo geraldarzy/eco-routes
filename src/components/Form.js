983844530e5b43f31ccc49560b17b9d11e400ae9
geraldarzy <geraldarzola@gmail.com>
2021-05-12 00:06:58 -0400

    comment out all the logic out of form; input onChange will now update MapLogic State

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,41 +6,40 @@ import {changeDestination} from '../actions/changeDestination';
 class Form extends React.Component{
     constructor(props){
         super();
-        this.state={
-            origin:'',
-            destination:''
-        }
+       
     }
 
-    handleChange = (e) =>{
-        //handles input change dynamically by producing a new obj with target.id 
-        //we can conver this to es6 with object.assign
-        let stateObject = function() {
-            let obj = {};
-            obj[e.target.id] = e.target.value;
-            return obj;
-        }
+    // handleChange = (e) =>{
+    //     //handles input change dynamically by producing a new obj with target.id 
+    //     //we can conver this to es6 with object.assign
+    //     let stateObject = function() {
+    //         let obj = {};
+    //         obj[e.target.id] = e.target.value;
+    //         return obj;
+    //     }
         
-        this.setState( stateObject );
-    }
+    //     this.setState( stateObject );
+    // }
 
-    handleSubmit = (e) =>{
-        e.preventDefault();
-        //call API to turn names into coordinates
-        console.log(`The Origin is ${this.state.origin} and the destination is ${this.state.destination}`)
-        //___________origin_______________________________________
-        let origin = this.state.origin.replace( / /g, '%20' );
-        let originurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + origin + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
-        fetch(originurl).then(resp=>resp.json()).then(json=>{
-            console.log(json.features[0].center);
-        })
-        //___________destination___________________________________
-        let destination = this.state.destination.replace( / /g, '%20' );
-        let destinationurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + destination + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
-        fetch(destinationurl).then(resp=>resp.json()).then(json=>{
-            console.log(json.features[0].center);
-        })
-    }
+    // handleSubmit = (e) =>{
+    //     e.preventDefault();
+    //     //call API to turn names into coordinates
+    //     console.log(`The Origin is ${this.state.origin} and the destination is ${this.state.destination}`)
+    //     //___________origin_______________________________________
+    //     let origin = this.state.origin.replace( / /g, '%20' );
+    //     let originurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + origin + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
+    //     fetch(originurl).then(resp=>resp.json()).then(json=>{
+    //         console.log(json.features[0].center);
+    //         this.props.changeOrigin(json.features[0].center)
+    //     })
+    //     //___________destination___________________________________
+    //     let destination = this.state.destination.replace( / /g, '%20' );
+    //     let destinationurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + destination + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
+    //     fetch(destinationurl).then(resp=>resp.json()).then(json=>{
+    //         console.log(json.features[0].center);
+    //         this.props.changeDestination(json.features[0].center)
+    //     })
+    // }
     render(){
         return (
             <form onSubmit={this.handleSubmit}>
@@ -56,6 +55,13 @@ class Form extends React.Component{
     }
 }
 
+const mapStateToProps=(state)=>{
+    return{
+        origin:state.origin,
+        destination:state.destination
+    }
+}
+
 const mapDispatchToProps = (dispatch)=>{
     return{
         changeOrigin:(origin)=>{
@@ -67,4 +73,4 @@ const mapDispatchToProps = (dispatch)=>{
     }
 }
 
-export default connect(null,mapDispatchToProps)(Form);
+export default connect(mapStateToProps,mapDispatchToProps)(Form);
