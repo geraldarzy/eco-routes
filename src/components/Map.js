6f6ad06b8a376f1a295666fafba7f9d5f52e489f
geraldarzy <geraldarzola@gmail.com>
2021-05-12 11:32:06 -0400

    define method to display lines on our map when there is an API response available; this now also rerenders everytime there is a new API response because new response=propchange

--- src/components/Map.js
+++ src/components/Map.js
@@ -33,7 +33,44 @@ class Map extends React.Component{
     }
     render(){
 
-        
+        //everything in here will rerender once state/prop is changed
+        //initially resp is null; how do we run this method once resp is equal to an object so that we can use that object to display lines on map
+        //how do we trigger this method everytime resp changes so that everytime resp changes we display new lines according to new resp
+        //re-render
+
+        //once there is an API response, use the response to display the lines, and fitBounds to lines
+        //once API response changes, our props changes, causing a re-render and therefore drawing new lines ;)
+        if(!!(this.props.resp)){
+            if(this.map.getSource('route')){
+                this.map.removeLayer('route');
+                this.map.removeSource('route');
+            }
+            this.map.addSource('route',{
+                'type':'geojson',
+                'data':{
+                    'type':'Feature',
+                    'properties': {},
+                    'geometry':{
+                        'type':'LineString',
+                        'coordinates':this.props.resp.routes[0].geometry.coordinates
+                    }
+                }
+            });
+            this.map.addLayer({
+                'id':'route',
+                'type':'line',
+                'source':'route',
+                'layout':{
+                    'line-join':'round',
+                    'line-cap': 'round'
+                },
+                'paint':{
+                    'line-color':'#888',
+                    'line-width':8
+                }
+            })
+        }
+
     return (
         <div id='map-container' class='map-container'>
 
