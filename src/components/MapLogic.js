6b8dde820bcbf0ce4b537ce4e9f93b4f35671c6f
geraldarzy <geraldarzola@gmail.com>
2021-05-12 01:36:01 -0400

    move fetch calls directly into handleSubmit method so that we can utilize async await; need to await location to coordinates in order for fetchDirections to run properly

--- src/components/MapLogic.js
+++ src/components/MapLogic.js
@@ -26,27 +26,6 @@ class MapLogic extends React.Component{
         this.setState( stateObject );
     }
 
-    locationToCoordinates = () =>{
-        //turn spaces into '%20' as per API URL 
-
-         //___________origin_______________________________________
-        let origin = this.state.origin.replace( / /g, '%20' );
-        let originurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + origin + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
-        fetch(originurl).then(resp=>resp.json()).then(json=>{
-            this.setState({
-                origin_coordinates:json.features[0].center
-            })
-        })
-
-        //___________destination___________________________________
-        let destination = this.state.destination.replace( / /g, '%20' );
-        let destinationurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + destination + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
-        fetch(destinationurl).then(resp=>resp.json()).then(json=>{
-            this.setState({
-                destination_coordinates:json.features[0].center
-            })
-        })
-    }
 
     fetchDirections=()=>{
         let url = `https://api.mapbox.com/directions/v5/mapbox/driving/${this.state.origin_coordinates[0]},${this.state.origin_coordinates[1]};${this.state.destination_coordinates[0]},${this.state.destination_coordinates[1]}?geometries=geojson&access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
@@ -57,11 +36,29 @@ class MapLogic extends React.Component{
         })
     }
 
-    handleSubmit=()=>{
+    handleSubmit= async ()=>{
         //turn location names into coordinates -done
+          //___________origin_______________________________________
+          let origin = this.state.origin.replace( / /g, '%20' );
+          let originurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + origin + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
+          await fetch(originurl).then(resp=>resp.json()).then(json=>{
+              this.setState({
+                  origin_coordinates:json.features[0].center
+              })
+          })
+  
+          //___________destination___________________________________
+          let destination = this.state.destination.replace( / /g, '%20' );
+          let destinationurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + destination + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
+          await fetch(destinationurl).then(resp=>resp.json()).then(json=>{
+              this.setState({
+                  destination_coordinates:json.features[0].center
+              })
+          })
+
+          
         //use coordinates to fetch directions -done
         //throw directions response into redux store -done in fetchDirection
-        this.locationToCoordinates();
         this.fetchDirections();
     }
 
