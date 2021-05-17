b07a3d681600a2bb839cf899afeb6732c8a9abfb
geraldarzy <geraldarzola@gmail.com>
2021-05-17 16:49:25 -0400

    add Router from react router dom

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import App from './App';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducer from './reducers/reducer'
+import {BrowserRouter as Router}  from 'react-router-dom';
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
+    <Router>
       <App store={store}/>
-    </React.StrictMode>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
