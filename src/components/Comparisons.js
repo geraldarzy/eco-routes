6e4aa2dd9a7cec76a27c697180eb94a3874b1dc3
geraldarzy <geraldarzola@gmail.com>
2021-05-17 23:27:16 -0400

    add comparisons component to compare our calculated carbon footprint with different things

---
+++ src/components/Comparisons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Comparisons extends React.Component{
+
+    //america: 6558mil metric tons/14.5trillion pounds /yr
+    //pets dogs:770kg /yr cats 310kg /yr
+    // avg human globably 4 tons /yr
+
+    render(){
+        return(
+            <div>
+                <h6>Compare your carbon footprint with ...</h6>
+            </div>
+        )
+    }
+}
+
+export default Comparisons
