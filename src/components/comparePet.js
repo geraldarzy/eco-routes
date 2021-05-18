bb5b6c640f46acdc25898466a6ed5f4f0116ba6a
geraldarzy <geraldarzola@gmail.com>
2021-05-18 13:19:01 -0400

    do the math to compare pets daily co2 to trips co2; (tripCo2/petsDailyCo2)*100 is the percent

--- src/components/comparePet.js
+++ src/components/comparePet.js
@@ -6,10 +6,22 @@ class Pet extends React.Component{
     // dogs: 1697.56 lbs / yr || cats: 683.43 lbs per year
     // dogsDaily: 4.65 | catsDaily: 1.87lbs daily
 
+    versusDailyDog = (tripCo2) =>{
+        //dogsDaily: 4.65lbs per day
+        let dogsDaily = 4.65;
+        return(((tripCo2/dogsDaily)*100).toFixed(2))
+    }
+
+    versusDailyCat = (tripCo2) =>{
+        //catsDaily: 1.87lbs per day
+        let catsDaily = 1.87;
+        return(((tripCo2/catsDaily)*100).toFixed(2))
+    }
+
     render(){
         return(
             <div>
-
+                Your trip is {this.versusDailyDog(this.props.tripCo2)}% of a dogs average daily carbon footprint and {this.versusDailyCat(this.props.tripCo2)}% of a cats average daily carbon footprint.
             </div>
         )
     }
