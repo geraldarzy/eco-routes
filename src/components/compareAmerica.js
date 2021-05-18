f529d14a5a6df41bcdb0409b2ffacd0470079652
geraldarzy <geraldarzola@gmail.com>
2021-05-18 13:22:39 -0400

    do the math comparison for America's daily co2 emission

--- src/components/compareAmerica.js
+++ src/components/compareAmerica.js
@@ -4,11 +4,16 @@ class America extends React.Component{
     
     //america: 6558mil metric tons => 14.5trillion pounds /yr
     //14.5 trillion lbs / 365 = 3.973 * 10^10 => 39,730,000,000 lbs daily
+    versusDailyAmerica = (tripCo2) =>{
+        //americaDaily = 39,730,000,000 lbs per day
+        let americaDaily = 39730000000;
+        return(((tripCo2/americaDaily)*100))
+    }
 
     render(){
         return(
             <div>
-                {this.props.tripCo2.toFixed(2)}lbs vs. 39,730,000,000lbs of co2 emitted by America Daily
+                Your trip is {this.versusDailyAmerica(this.props.tripCo2)}% of America's average daily carbon footprint.
             </div>
         )
     }
