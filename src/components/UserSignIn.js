f0e3cabbc55fdbfceda98b7494007827f8574ba5
geraldarzy <geraldarzola@gmail.com>
2021-05-19 21:42:56 -0400

    add SignIn form to SignIn route

--- src/components/UserSignIn.js
+++ src/components/UserSignIn.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 class UserSignIn extends React.Component{
 
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        let email = e.target.children.email.value;
+        let password = e.target.children.password.value;
+    }
+
     render(){
         return(
             <div>
-                sign in here
+                <form onSubmit={this.handleSubmit}>
+                    <label for='email'>Email:</label>
+                    <input type='text' id='email'></input>
+                    <br/>
+                    <label for='email'>Password:</label>
+                    <input type='text' id='password'></input>
+                    <br/>
+                    <input type='submit'></input>
+                </form>
             </div>
         )
     }
