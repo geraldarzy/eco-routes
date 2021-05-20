c35567f668ea94d05900a23a6cc0aed8a9c2a98a
geraldarzy <geraldarzola@gmail.com>
2021-05-20 01:47:45 -0400

    add MemberAccess component; this component is responsible of showing features only accesible if signed in

---
+++ src/components/MemberAccess.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MemberAccess extends React.Component{
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default MemberAccess;
