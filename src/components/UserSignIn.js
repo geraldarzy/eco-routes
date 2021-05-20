b03ce8ab96219e337cd1b48e458affd831b4e5d1
geraldarzy <geraldarzola@gmail.com>
2021-05-19 21:23:24 -0400

    make signin/up components

---
+++ src/components/UserSignIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class UserSignIn extends React.Component{
+
+    render(){
+        return(
+            <div>
+                sign in here
+            </div>
+        )
+    }
+}
+
+export default UserSignIn;
---
+++ src/components/UserSignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class UserSignUp extends React.Component{
+
+    render(){
+        return(
+            <div>
+                sign up here
+            </div>
+        )
+    }
+}
+
+export default UserSignUp;
