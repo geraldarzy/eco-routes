fd5fb0f66bfeb65dac22e93aae130df8de0b9ea3
geraldarzy <geraldarzola@gmail.com>
2021-05-21 19:00:07 -0400

    if trips is populated, set state = to trips

--- src/components/TripBook.js
+++ src/components/TripBook.js
@@ -3,6 +3,7 @@ import {useEffect, useState} from 'react'
 function TripBook() {
     const [currentUser, setCurrentUser] = useState(null);
     const [error, setError] = useState(null);
+    const [trips, setTrips] = useState(null);
 
     const fetchCurrentUser= async ()=>{
         // TODO: check if there'a token for the logged in user
@@ -32,8 +33,10 @@ function TripBook() {
         //get users tripbook
         fetch(`http://localhost:3000//trip_books/${currentUser.trip_book_id}`).then(resp=>resp.json()).then(response=>{
             if(response.message){
-                setError(response.message);
+                return setError(response.message);
             }
+            setTrips(response.tripbook_trips);
+
         })  
 
 
