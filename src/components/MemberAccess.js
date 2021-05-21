bf82b019ec961c7b81ed4d5443fe56601bdd84d7
geraldarzy <geraldarzola@gmail.com>
2021-05-21 17:40:31 -0400

    when button addTrip, POST request to backend to instantiate a new trip with our trip_book

--- src/components/MemberAccess.js
+++ src/components/MemberAccess.js
@@ -1,9 +1,12 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react';
+import { useSelector } from 'react-redux';
 
 function MemberAccess(){
 
     const [currentUser, setCurrentUser] = useState(null);
 
+    const directionsResponse = useSelector(state=>state.resp);
+    const test = 'hello'
     const fetchCurrentUser= async ()=>{
         // TODO: check if there'a token for the logged in user
         // GET /me
@@ -17,36 +20,56 @@ function MemberAccess(){
             .then((r) => r.json())
             .then((user) => {
                 // set the user in state
-                console.log({user})
                 setCurrentUser(user);
             });
         }
+    }
+
+    const calculateCarbonFootprint=(mpg=25,dist)=>{
+        // dist is in meters, mpg is in miles
+        // so first convers dist from meters to miles
+        let dist_in_miles = (dist/1609.344)
+
+        // dist_in_miles/mpg = gallons_used
+        // 19.60 pounds of co2/gallon => gallons_used * 19.6 = pounds of co2
+        let co2_produced = dist_in_miles/mpg *19.6;
+        return co2_produced;
     }
 
     useEffect( () => {
         fetchCurrentUser();
     }, []);
 
-    const addTrip = () =>{
-        debugger;
-        console.log('adding trip');
-        // /trip_books/:trip_book_id/trips
-        // t.string "origin"
-        // t.string "destination"
-        // t.string "distance"
-        // t.string "trip_co2"
-        // t.integer "trip_book_id"
-        
-    }
+    
+    if(currentUser){
 
-        if(this.props.currentUser){
+       
+
+        const addTrip = () =>{
+            // t.string "origin"
+            // t.string "destination"
+            // t.string "distance"
+            // t.string "trip_co2"
+            // t.integer "trip_book_id"
+            fetch(`http://localhost:3000//trip_books/${currentUser.trip_book_id}/trips`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ trip_book_id: currentUser.trip_book_id, origin: directionsResponse.waypoints[0].name, destination: directionsResponse.waypoints[1].name, distance: directionsResponse.routes[0].distance, trip_co2: calculateCarbonFootprint(25,directionsResponse.routes[0].distance)}),
+                })
+            .then(resp=>resp.json()).then(response=>{
+                //do something after adding trip
+            })
+         }
+
             return(
                 <div>
                     <p>Add this trip to your 'Trips' to keep track of your carbon footprint.</p>
-                    <button onClick={this.addTrip} >Add Trip</button>
+                    <button onClick={addTrip} >Add Trip</button>
                 </div>
             )
-        }
+    }
         return (
             <div>
                 <p>Want to keep track of your carbon footprint? Sign In Now!</p>
