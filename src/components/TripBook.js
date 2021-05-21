5424d6b9a9d277eaa25ac195c8e944c4691fdc80
geraldarzy <geraldarzola@gmail.com>
2021-05-21 14:01:37 -0400

    when component mounts, fetchCurrent user if token exists in local storage

--- src/components/TripBook.js
+++ src/components/TripBook.js
@@ -1,9 +1,48 @@
-function TripBook() {
+import {useEffect, useState} from 'react'
 
-  
-    return (
+function TripBook() {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    const fetchCurrentUser= async ()=>{
+        // TODO: check if there'a token for the logged in user
+        // GET /me
+        const token = localStorage.getItem("token");
+        if (token) {
+           await fetch("http://localhost:3000/me", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+            .then((r) => r.json())
+            .then((user) => {
+                // set the user in state
+                console.log({user})
+                setCurrentUser(user);
+            });
+        }
+    }
+
+    useEffect( () => {
+        fetchCurrentUser();
+      }, []);
+
+    
+    if(currentUser){
+        debugger;
+        //if a user is logged in
+        //get users tripbook
+        
+
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+    return(
         <div>
-            
+            not logged in
         </div>
     )
 }
