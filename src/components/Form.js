be22426235ada6c7e06895b68c29055812ae8c0f
geraldarzy <geraldarzola@gmail.com>
2021-05-24 14:05:05 -0400

    add react-select package for Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,43 +1,13 @@
 import React from 'react';
-
-
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css'
 class Form extends React.Component{
     constructor(props){
         super();
        
     }
 
-    // handleChange = (e) =>{
-    //     //handles input change dynamically by producing a new obj with target.id 
-    //     //we can conver this to es6 with object.assign
-    //     let stateObject = function() {
-    //         let obj = {};
-    //         obj[e.target.id] = e.target.value;
-    //         return obj;
-    //     }
-        
-    //     this.setState( stateObject );
-    // }
-
-    // handleSubmit = (e) =>{
-    //     e.preventDefault();
-    //     //call API to turn names into coordinates
-    //     console.log(`The Origin is ${this.state.origin} and the destination is ${this.state.destination}`)
-    //     //___________origin_______________________________________
-    //     let origin = this.state.origin.replace( / /g, '%20' );
-    //     let originurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + origin + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
-    //     fetch(originurl).then(resp=>resp.json()).then(json=>{
-    //         console.log(json.features[0].center);
-    //         this.props.changeOrigin(json.features[0].center)
-    //     })
-    //     //___________destination___________________________________
-    //     let destination = this.state.destination.replace( / /g, '%20' );
-    //     let destinationurl =`https://api.mapbox.com/geocoding/v5/mapbox.places/` + destination + `.json?access_token=`+process.env.REACT_APP_MAPBOX_TOKEN;
-    //     fetch(destinationurl).then(resp=>resp.json()).then(json=>{
-    //         console.log(json.features[0].center);
-    //         this.props.changeDestination(json.features[0].center)
-    //     })
-    // }
+    
     render(){
         return (
             <div>
