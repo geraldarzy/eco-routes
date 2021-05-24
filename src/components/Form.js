255427a140bfe821523ad3f9e95ae2b4e2ef74a6
geraldarzy <geraldarzola@gmail.com>
2021-05-24 14:26:37 -0400

    remove react-select, that was for a dropdown, we need autocomplete not dropdown

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 class Form extends React.Component{
     constructor(props){
         super();
        
     }
 
+
+
     
     render(){
+        const trial = [
+            {label:'Apple'},
+            {label:'Facebook'},
+            {label:'Mapbox'},
+            {label:'Google'},
+            {label:'Walmart'}
+        ]
         return (
             <div>
                 <input onChange={this.props.handleInputChange} id='origin'type='text' placeholder='Origin'></input>
